37ec3658c10f2ec5b793865ba00ed9ee8fae20af
Justin Rosenthal <rosenthalj@google.com>
2017-05-04 18:04:32 -0700

    Prevent TopicPill background from clearing when in highlighted cell

--- Hubbub/TopicPill.swift
+++ Hubbub/TopicPill.swift
@@ -13,7 +13,7 @@ class TopicPill: UILabel {
     override init(frame: CGRect) {
         super.init(frame: frame)
         textColor = #colorLiteral(red: 0, green: 0.5921568627, blue: 0.6549019608, alpha: 1)
-        backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.9647058824, blue: 0.9764705882, alpha: 1)
+        layer.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.9647058824, blue: 0.9764705882, alpha: 1).cgColor
         
         textAlignment = .center
         font = UIFont.boldSystemFont(ofSize: 12)
