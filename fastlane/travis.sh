f54bc91f23ba4c970aeb9eadb17244aee041bed3
Judy Tuan <judytuna@gmail.com>
2017-05-08 20:41:25 -0700

    looking for codesigning identities

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -3,6 +3,8 @@
 # if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   git status
   ls -al certificates/
+  security find-identity -v -p codesigning
   fastlane travis_deploy
+  security find-identity -v -p codesigning
   exit $?
 # fi
