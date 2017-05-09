dd9fc7dc8ca5be9763014345bab3ae79b720f7fd
Judy Tuan <judytuna@gmail.com>
2017-05-08 22:00:43 -0700

    different ruby that travis has installed already i hope

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -4,6 +4,8 @@
   git status
   ls -al certificates/
   security find-identity -v -p codesigning
+  which fastlane
+  fastlane --version
   fastlane travis_deploy
   security find-identity -v -p codesigning
   exit $?
