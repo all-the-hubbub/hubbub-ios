e62a523e03738b50610078710d407f60979f6fbf
Justin Rosenthal <rosenthalj@google.com>
2017-05-17 14:37:29 -0700

    Rename dev to beta, create new dev environment

--- Hubbub/Config.swift
+++ Hubbub/Config.swift
@@ -9,35 +9,44 @@
 import Foundation
 
 struct Config {
-    // Always the real id so the upgrade dialog can properly open the App Store
-    static let AppStoreID = "id1234046078"
+    static let AppStoreID = env(
+        dev: "",
+        beta: "id1234046078",
+        prod: "id1234046078"
+    )
 
     static let FirebasePlistName = env(
         dev: "GoogleService-Info-Dev",
-        prod: "GoogleService-Info"
+        beta: "GoogleService-Info-Beta",
+        prod: "GoogleService-Info-Prod"
     )
 
     static let StaticHost = env(
-        dev: "hubbub-staging.firebaseapp.com",
+        dev: "<PROJECT_ID>.firebaseapp.com",
+        beta: "hubbub-staging.firebaseapp.com",
         prod: "hubbub-159904.firebaseapp.com"
     )
 
     static let FunctionsHost = env(
-        dev: "us-central1-hubbub-staging.cloudfunctions.net",
+        dev: "us-central1-<PROJECT_ID>.cloudfunctions.net",
+        beta: "us-central1-hubbub-staging.cloudfunctions.net",
         prod: "us-central1-hubbub-159904.cloudfunctions.net"
     )
 
     static let GitHubOAuthClientID = env(
-        dev: "8047073cafed8937f908",
+        dev: "",
+        beta: "8047073cafed8937f908",
         prod: "077cb2f4568e245a97eb"
     )
 }
 
-func env<T>(dev development: T, prod production: T) -> T {
+func env<T>(dev development: T, beta: T, prod production: T) -> T {
     var v: T!
 
     #if ENV_DEV
         v = development
+    #elseif ENV_BETA
+        v = beta
     #else
         v = production
     #endif
